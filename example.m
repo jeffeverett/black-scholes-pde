42ebccf512eefd52f3b388fa325f59b9a1d509cb
Jeff Everett <everett.jeff.w@gmail.com>
2018-06-10 21:44:26 -0700

    Improved example

--- example.m
+++ example.m
@@ -1,6 +1,6 @@
 %% Definite initial constants
-N = 1000;
-M = 250;
+N = 4000;
+M = 1000;
 Smin = 0.4; % must be greater than 0 to ensure ln(Smin/K) is defined
 Smax = 1000;
 T = 1;
@@ -8,25 +8,26 @@ K = 10;
 volatility = 0.4;
 r = 0.02;
 is_call = 1;
-d = 0.04;
+d = 0;
 
-%% Generate and plot surface
+%% Generate surface
 [t_vals,S_vals,surface] = black_scholes_cov_explicit(N,M,Smin,Smax,T,K,volatility,r,d,is_call);
 
-S_checks = linspace(0,1.5*K);
-t_checks = linspace(0,1);
+S_check = linspace(0,1.5*K,20);
+t_check = linspace(0,T,20);
 
-surf_check = zeros(size(t_checks,2),size(S_checks,2));
-for i=1:size(t_checks,2)
-    for j=i:size(S_checks,2)
-        surf_check(i,j) = interp2(S_vals,t_vals,surface,S_checks(j),t_checks(i));
+surf_check = zeros(size(S_check,2),size(t_check,2));
+for i=1:size(t_check,2)
+    for j=1:size(S_check,2)
+        surf_check(i,j) = interp2(S_vals,t_vals,surface,S_check(j),t_check(i));
     end
 end
 
+%% Plot surface
 surf(S_checks,fliplr(t_checks),surf_check)
 xlabel('Stock Price')
 ylabel('Time Until Maturity')
-title('Generated Surface')
+title('Example Surface')
 
 %% Interpolate single point and display it
 S_check = 30;
